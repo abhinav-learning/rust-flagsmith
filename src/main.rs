ca7ac5615d6c8d237fc53b1dc5d1f89084eb216d
Abhinav Nath Gupta <abhi.aec89@gmail.com>
2024-11-24 23:48:54 +0530

    Added the Routes

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,20 @@ fn index() -> &'static str {
     "Hello, world! This is rocket"
 }
 
+#[get("/ping")]
+fn ping() -> &'static str {
+    "Hello, world! This is ping"
+}
+
+#[get("/beta")]
+fn beta() -> &'static str {
+    "Hello, world! This is beta"
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/", routes![ping])
+        .mount("/", routes![beta])
 }
