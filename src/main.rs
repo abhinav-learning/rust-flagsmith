6b6c4806dbd5d2bba1f619a79b46da34b3a33b45
Abhinav Nath Gupta <abhi.aec89@gmail.com>
2024-11-25 23:16:29 +0530

    Added the ability to override the port from config

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 #[macro_use]
 extern crate rocket;
 extern crate dotenv;
-
 use dotenv::dotenv;
+use rocket::custom;
 use std::env;
 
 #[get("/")]
@@ -21,38 +21,49 @@ fn beta() -> &'static str {
 }
 
 // Configuration struct to hold env variables
-struct Config {
+struct EnvConfig {
     rate_limit: u32,
-    app_name: String,
-    environment: String,
+    redis_url: String,
+    fsk: String,
+    port: u16,
 }
 
-
-impl Config {
-    fn from_env() -> Config {
-        Config {
+impl EnvConfig {
+    fn from_env() -> EnvConfig {
+        EnvConfig {
             rate_limit: env::var("RATE_LIMIT")
                 .unwrap_or_else(|_| "100".to_string())
                 .parse()
                 .unwrap_or(100),
-            app_name: env::var("APP_NAME")
-                .unwrap_or_else(|_| "DefaultApp".to_string()),
-            environment: env::var("ENVIRONMENT")
-                .unwrap_or_else(|_| "development".to_string()),
+            port: env::var("PORT")
+                .unwrap_or_else(|_| "8080".to_string())
+                .parse()
+                .unwrap_or(8080),
+            redis_url: env::var("REDIS_URL").unwrap_or_else(|_| "localhost:6379".to_string()),
+            fsk: env::var("FLAGSMITH_ENVIRONMENT_KEY")
+                .unwrap_or_else(|_| "FLAGSMITH_ENVIRONMENT_KEY".to_string()),
         }
     }
 }
 
-#[launch]
-fn rocket() -> _ {
+#[rocket::main]
+async fn main() {
     dotenv().ok();
     // Load configuration
-    let config = Config::from_env();
-    println!("Starting {} in {} environment", 
-             config.app_name, 
-             config.environment);
-    rocket::build()
-        .mount("/", routes![index])
-        .mount("/", routes![ping])
-        .mount("/", routes![beta])
+    let env_config = EnvConfig::from_env();
+    let config = custom(rocket::Config {
+        port: env_config.port, // Change this to your desired port number
+        address: std::net::Ipv4Addr::new(127, 0, 0, 1).into(),
+        ..rocket::Config::debug_default()
+    });
+    // println!(
+    //     "Starting {} in {} environment",
+    //     config.app_name, config.environment
+    // );
+    config
+        .mount("/", rocket::routes![index])
+        .mount("/", rocket::routes![ping])
+        .mount("/", rocket::routes![beta])
+        .launch()
+        .await;
 }
