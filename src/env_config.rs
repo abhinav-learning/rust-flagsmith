f9659b9ed241809715587e4e98debeb17652e81e
Abhinav <tech01@publiqverse.io>
2024-11-27 18:26:42 +0530

    Redis Endpoint Env Config

--- src/env_config.rs
+++ src/env_config.rs
@@ -3,7 +3,7 @@ use std::env;
 // Configuration struct to hold env variables
 pub struct EnvConfig {
     // pub rate_limit: u32,
-    // pub redis_url: String,
+    pub redis_url: String,
     // pub fsk: String,
     pub port: u16,
 }
@@ -19,7 +19,7 @@ impl EnvConfig {
                 .unwrap_or_else(|_| "8080".to_string())
                 .parse()
                 .unwrap_or(8080),
-            // redis_url: env::var("REDIS_URL").unwrap_or_else(|_| "localhost:6379".to_string()),
+            redis_url: env::var("REDIS_URL").unwrap_or_else(|_| "localhost:6379".to_string()),
             // fsk: env::var("FLAGSMITH_ENVIRONMENT_KEY")
             //     .unwrap_or_else(|_| "FLAGSMITH_ENVIRONMENT_KEY".to_string()),
         }
