7f37cad38b470739335020a6c90af507f0614118
Abhinav <tech01@publiqverse.io>
2024-11-27 18:52:23 +0530

    Added the Handler Functions for Redis Client

--- src/controller.rs
+++ src/controller.rs
@@ -1,3 +1,33 @@
-pub fn get_redis_client() -> bool {
-    true
+use redis::{Client, Commands};
+
+pub trait RedisClientImpl {
+    fn connect(&self) -> Result<redis::Connection, redis::RedisError>;
+    fn set(&mut self, key: String, value: String) -> Result<(), redis::RedisError>;
+    fn get(&mut self, key: String) -> Result<Option<String>, redis::RedisError>;
+}
+
+pub struct RedisClient {
+    client: Client,
+}
+
+impl RedisClientImpl for RedisClient {
+    fn connect(&self) -> Result<redis::Connection, redis::RedisError> {
+        self.client.get_connection()
+    }
+
+    fn set(&mut self, key: String, value: String) -> Result<(), redis::RedisError> {
+        let mut con = self.connect()?;
+        con.set(key, value)
+    }
+
+    fn get(&mut self, key: String) -> Result<Option<String>, redis::RedisError> {
+        let mut con = self.connect()?;
+        con.get(key)
+    }
+}
+
+pub fn create_client(redis_url: &str) -> Result<RedisClient, redis::RedisError> {
+    Ok(RedisClient {
+        client: Client::open(redis_url)?,
+    })
 }
