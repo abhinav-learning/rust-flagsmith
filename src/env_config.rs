439f23788782df7348c0ae913ba31eb6fe1a2001
Abhinav <tech01@publiqverse.io>
2024-11-27 22:38:22 +0530

    Enabled Env Variables for FlagSmith

--- src/env_config.rs
+++ src/env_config.rs
@@ -2,19 +2,19 @@ use std::env;
 
 // Configuration struct to hold env variables
 pub struct EnvConfig {
-    // pub rate_limit: u32,
+    pub rate_limit: u32,
     pub redis_url: String,
-    // pub fsk: String,
     pub port: u16,
+    pub flagsmith_environment_key: String,
 }
 
 impl EnvConfig {
     pub fn from_env() -> EnvConfig {
         EnvConfig {
-            // rate_limit: env::var("RATE_LIMIT")
-            //     .unwrap_or_else(|_| "100".to_string())
-            //     .parse()
-            //     .unwrap_or(100),
+            rate_limit: env::var("RATE_LIMIT")
+                .unwrap_or_else(|_| "100".to_string())
+                .parse()
+                .unwrap_or(100),
             port: env::var("PORT")
                 .unwrap_or_else(|_| "8080".to_string())
                 .parse()
@@ -23,8 +23,8 @@ impl EnvConfig {
                 "redis://{}/",
                 env::var("REDIS_URL").unwrap_or_else(|_| "localhost:6379".to_string())
             ),
-            // fsk: env::var("FLAGSMITH_ENVIRONMENT_KEY")
-            //     .unwrap_or_else(|_| "FLAGSMITH_ENVIRONMENT_KEY".to_string()),
+            flagsmith_environment_key: env::var("FLAGSMITH_ENVIRONMENT_KEY")
+                .unwrap_or_else(|_| "FLAGSMITH_ENVIRONMENT_KEY".to_string()),
         }
     }
 }
--- src/feature_flags.rs
+++ src/feature_flags.rs
@@ -1,15 +1,12 @@
+use crate::env_config::EnvConfig;
 use flagsmith::{Flag, Flagsmith, FlagsmithOptions};
-use std::env;
 
 pub fn fetch_feature_flag(flag_name: &str) -> Flag {
+    let env: EnvConfig = EnvConfig::from_env();
     let options = FlagsmithOptions {
         ..Default::default()
     };
-    let flagsmith = Flagsmith::new(
-        env::var("FLAGSMITH_SERVER_SIDE_ENVIRONMENT_KEY")
-            .expect("FLAGSMITH_SERVER_SIDE_ENVIRONMENT_KEY not found in environment"),
-        options,
-    );
+    let flagsmith = Flagsmith::new(env.flagsmith_environment_key, options);
     let flags = flagsmith.get_environment_flags().unwrap();
     flags.get_flag(flag_name).unwrap_or_default()
 }
